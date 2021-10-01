ad9da2ac214f5b0161a74ad71bd1f22afcff5820
shubhampatte <shubhampatte@gmail.com>
2021-10-01 23:23:27 +0530

    [shubham]: co-ordinates of a line

--- lineComparison.java
+++ lineComparison.java
@@ -3,7 +3,11 @@ public class lineComparison
 	public static void main(String args[])
 	{
 
-		System.out.println("Welcome to Line Comparison Problem");
+		int x1 = 4, x2 = 8;
+		int y1 = 3, y2 = 9;
+		
+		int length = (int) Math.sqrt(((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)));
+		System.out.println("length of a line: "+length);
 	}
 
 }
